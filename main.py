176087badfe36fe93cf2eb7524eb3a784bfbfa75
Atharva <atharva.malik@gmail.com>
2024-07-09 16:03:26 +1000

    Made basic movement

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from pacman import *
+from os import system, name
+import keyboard, time
+
+def clear():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
 board = Board()
-for i in board.pacman_map:
-    for j in i:
-        print(j+" ", end="")
-    print()
+while True:
+    clear()
+    print(board)
+    if keyboard.is_pressed("w"):
+        board.pac.direction = 2
+        keyboard.press_and_release("backspace")
+    if keyboard.is_pressed("a"):
+        board.pac.direction = 4
+        keyboard.press_and_release("backspace")
+    if keyboard.is_pressed("s"):
+        board.pac.direction = 1
+        keyboard.press_and_release("backspace")
+    if keyboard.is_pressed("d"):
+        board.pac.direction = 3
+        keyboard.press_and_release("backspace")
+    board.update()
+    time.sleep(0.1)
--- pacman.py
+++ pacman.py
@@ -1,11 +1,12 @@
-pacman = ["ᗤ", "ᗣ", "ᗧ", "ᗢ"]
-blinky = ["#", "%"]
-pinky = ["@", "+"]
-Inky = ["&", "^"]
-clyde = ["$", "€"]
+import copy
+
+blinky = ["Β", "β"]
+pinky = ["Φ", "φ"]
+Inky = ["Ν", "ν"] # The greek letter Iota looked too much like the map which is why I had to use Nu
+clyde = ["$", "€"] # Lore: Clyde is different from everyone else that's his symbol is special 
 class Board:
     def __init__(self):
-        self.map_size = (28, 31)
+        self.map_size = (31, 28)
         self.pacman_map = [
             ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
             ["|", "*", "*", "*", "*", "*", "*", "*", "*", "*", "|", "|", "*", "*", "*", "*", "|", "|", "*", "*", "*", "*", "*", "*", "*", "*", "*", "|"],
@@ -30,7 +31,7 @@ class Board:
             [" ", " ", "|", "*", "|", "|", "*", "|", "|", "*", "*", "*", "*", "|", "|", "*", "*", "*", "*", "|", "|", "*", "|", "|", "*", "|", " ", " "],
             [" ", " ", "|", "*", "|", "|", "*", "|", "-", "-", "-", "|", "*", "|", "|", "*", "|", "-", "-", "-", "|", "*", "|", "|", "*", "|", " ", " "],
             ["-", "-", "-", "*", "|", "|", "*", "|", "-", "-", "-", "|", "*", "-", "-", "*", "|", "-", "-", "-", "|", "*", "|", "|", "*", "-", "-", "-"],
-            ["|", "*", "*", "*", "|", "|", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "|", "|", "*", "*", "*", "|"],
+            ["|", "*", "*", "*", "|", "|", "*", "*", "*", "*", "*", "*", "*", "P", "*", "*", "*", "*", "*", "*", "*", "*", "|", "|", "*", "*", "*", "|"],
             ["|", "*", "|", "-", "-", "-", "-", "-", "-", "|", "*", "|", "-", "-", "-", "-", "|", "*", "|", "-", "-", "-", "-", "-", "-", "|", "*", "|"],
             ["|", "*", "|", "-", "-", "-", "-", "-", "-", "|", "*", "|", "-", "-", "-", "-", "|", "*", "|", "-", "-", "-", "-", "-", "-", "|", "*", "|"],
             ["|", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "|", "|", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "|"],
@@ -39,4 +40,79 @@ class Board:
             ["|", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "|"],
             ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
         ]
-        self.actual_map = self.pacman_map.copy()
+        self.actual_map = copy.deepcopy(self.pacman_map)
+        self.score = 0
+        self.pac = PacMan()
+        self.spawn()
+    
+    def spawn(self):
+        for i in range(len(self.actual_map)):
+            for j in range(len(self.actual_map[i])):
+                if self.actual_map[i][j] == "P":
+                    self.pac.pos = [i, j]
+                    self.actual_map[i][j] = self.pac.pacman[self.pac.direction]
+                    break
+    
+    def move(self):
+        #! TODO: UPDATE SCORE!
+        walls = ["|", "-", " "] # The characters that represent walls
+        position = self.pac.pos.copy()
+        direction = self.pac.direction
+        if direction > 2:
+            # Travelling left or right
+            if direction == 3:
+                # Travelling right
+                if position[1] == self.map_size[1]-1:
+                    self.pac.pos[1] = 0
+                    self.actual_map[position[0]][position[1]] = " "
+                    self.actual_map[position[0]][0] = self.pac.pacman[3]
+                else:
+                    if self.pacman_map[position[0]][position[1] + 1] not in walls:
+                        self.pac.pos[1] += 1
+                        self.actual_map[position[0]][position[1]] = " "
+                        self.actual_map[position[0]][position[1] + 1] = self.pac.pacman[3]
+            elif direction == 4:
+                # Travelling left
+                if position[1] == 0:
+                    self.pac.pos[1] = self.map_size[1]-1
+                    self.actual_map[position[0]][position[1]] = " "
+                    self.actual_map[position[0]][(self.map_size[1]-1)] = self.pac.pacman[3]
+                else:
+                    if self.pacman_map[position[0]][position[1] - 1] not in walls:
+                        self.pac.pos[1] -= 1
+                        self.actual_map[position[0]][position[1]] = " "
+                        self.actual_map[position[0]][position[1] - 1] = self.pac.pacman[4]
+        elif direction <= 2 and direction > 0:
+            # Travelling up or down
+            if direction == 1:
+                # Travelling down
+                if self.pacman_map[position[0] + 1][position[1]] not in walls:
+                    self.pac.pos[0] += 1
+                    self.actual_map[position[0]][position[1]] = " "
+                    self.actual_map[position[0] + 1][position[1]] = self.pac.pacman[1]
+            elif direction == 2:
+                # Travelling up
+                if self.pacman_map[position[0] - 1][position[1]] not in walls:
+                    self.pac.pos[0] -= 1
+                    self.actual_map[position[0]][position[1]] = " "
+                    self.actual_map[position[0] - 1][position[1]] = self.pac.pacman[2]
+    
+    def update(self):
+        # Move pacman
+        self.move()
+    
+    def __str__(self):
+        # Print the map
+        out = ""
+        for i in self.actual_map:
+            for j in i:
+                out += j + " "
+            out += "\n"
+        return out
+
+
+class PacMan:
+    def __init__(self):
+        self.pacman = ["0", "ᗣ", "ᗢ", "ᗧ", "ᗤ"]
+        self.direction = 0 # The index of the appropriate sprite in the pacman list
+        self.pos = []
